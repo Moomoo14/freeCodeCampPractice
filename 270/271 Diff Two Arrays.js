c9485937b09fa1a5951f0ebc0455ad0bb98e6a29
Pixzero <gralus@outlook.com>
2017-08-15 19:38:20 +0800

    add 271

--- 270/271 Diff Two Arrays.js
+++ 270/271 Diff Two Arrays.js
@@ -1,7 +1,9 @@
 function diff (arr1, arr2) {
-  var newArr = [];
-  // Same, same; but different.
-  return newArr;
+  return arr1.filter(function (v) {
+    return arr2.indexOf(v) == -1;
+  }).concat(arr2.filter(function (v) {
+    return arr1.indexOf(v) == -1;
+  }));
 }
 
 diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
